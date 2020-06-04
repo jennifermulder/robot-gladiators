cdebacacebaf520a47a9f064e930fd009f9dfae5
J Mulder <jlmulder2014@gmail.com>
2020-06-04 00:53:15 -0700

    high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -42,6 +42,7 @@ var fight = function(enemy) {
     // keep track of who goes first
     var isPlayerTurn = true;
 
+    // not the players turn
     if (Math.random() > 0.5) {
       isPlayerTurn = false;
     } 
@@ -164,19 +165,27 @@ var startGame = function() {
 
   // function to end the entire game
   var endGame = function() {
-    // if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-      window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    window.alert("The game has now ended. Let's see how you did!");
+
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+      highScore = 0;
+    }
+    // if player have more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+      localStorage.setItem("highscore", playerInfo.money);
+      localStorage.setItem("name", playerInfo.name);
+
+      alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     } 
     else {
-      window.alert("You've lost your robot in battle.");
+      alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
+
     // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
-
- // issue is here? when confirming that I want to play again, I starts me off at round 1   
- // skipping and canceling, the game will end and say "good bye comback soon" Game will then say welcome to round 2
     if (playAgainConfirm) {
     // restart the game
     startGame();
